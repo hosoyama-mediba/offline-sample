3b9b7aaf6c3744c479b81d36bf0b313a79c2c273
[mediba] Terunobu Hosoyama <hosoyama@mediba.jp>
2017-06-28 11:32:46 +0900

    add sw.

---
+++ docs/sw.js
@@ -0,0 +1,31 @@
+const cacheId = '1498617127673';
+const fallbackUrl = `/offline.html?_cc=${cacheId}`;
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheId)
+        .then(cache => cache.addAll([fallbackUrl]))
+        .then(() => self.skipWaiting()),
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys()
+        .then(keys => Promise.all(
+            keys.map((key) => {
+                if (key !== cacheId) {
+                    return caches.delete(key);
+                }
+                return undefined;
+            }),
+        ))
+        .then(() => self.clients.claim()),
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if (!navigator.onLine) {
+        event.respondWith(caches.match(fallbackUrl));
+    }
+});
